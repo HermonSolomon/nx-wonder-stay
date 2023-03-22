d2aad0093c96d99f05a686cbed4c8b59b0d75731
Hermon <hermons47@gmail.com>
2023-03-22 13:02:22 +0000

    create sanity schemas

--- apps/sanity-wonder-stay/schemas/index.ts
+++ apps/sanity-wonder-stay/schemas/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import host from './host'
+import person from './person'
+import property from './property'
+import propertyImage from './propertyImage'
+import review from './review'
+import traveler from './traveler'
+
+export const schemaTypes = [property, propertyImage, review, traveler, person, host]
