462964f6be986e60b5e9dbef6372070710011971
Hermon <hermons47@gmail.com>
2023-03-24 12:49:16 +0000

    add property [slug], add public images, extend property types

--- apps/wonder-stay/pages/property/[slug].tsx
+++ apps/wonder-stay/pages/property/[slug].tsx
@@ -70,62 +70,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   };
 };
 
-// export const getServerSideProps = () => async (pageContext) => {
-//   const pageSlug = pageContext.query?.slug;
-
-//   const query = `*[_type == "property" && slug.current == $pageSlug][0] {
-//     title,
-//     location,
-//     propertyType,
-//     mainImage,
-//     images,
-//     pricePerNight,
-//     beds,
-//     bedrooms,
-//     description,
-//     host->{
-//         _id,
-//         name,
-//         slug,
-//         image,
-//     },
-//     reviews[]{
-//         ...,
-//         traveler->{
-//             _id,
-//             name,
-//             slug,
-//             image
-//         }
-//     }
-//   }`;
-
-//   const property = await sanityClient.fetch(query, { pageSlug });
-
-//   if (!property) {
-//     return {
-//       props: null,
-//       notFound: true,
-//     };
-//   } else {
-//     return {
-//       props: {
-//         title: property.title,
-//         location: property.location,
-//         propertyType: property.propertyType,
-//         mainImage: property.mainImage,
-//         images: property.images,
-//         pricePerNight: property.pricePerNight,
-//         beds: property.beds,
-//         bedrooms: property.bedrooms,
-//         description: property.description,
-//         host: property.host,
-//         reviews: property.reviews,
-//       },
-//     };
-//   }
-// };
-
 interface Props {
   property: Properties;
 }
